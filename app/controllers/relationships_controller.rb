b816df946807d82886d293571a4d5a7ede73d6fc
kant9612 <kanstephentakayama@gmail.com>
2021-02-05 12:43:42 +0000

    [finish]フォロー機能追加

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -10,10 +10,10 @@ class RelationshipsController < ApplicationController
   end
   def create
     current_user.follow(params[:user_id])
-		redirect_to root_path
+		redirect_back(fallback_location:root_path)
   end
   def destroy
     current_user.unfollow(params[:user_id])
-		redirect_to root_path
+		redirect_back(fallback_location:root_path)
   end
 end
